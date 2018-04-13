6a6beb94c843b9be5ba3e6ba3fd2058e0067a5aa
Lars Graubner <mail@larsgraubner.de>
2018-04-13 12:19:06 +0200

    adjust interval

--- index.js
+++ index.js
@@ -1,104 +1,120 @@
 import {
-	NativeModules,
-	NativeEventEmitter,
-	DeviceEventEmitter,
-	NativeAppEventEmitter,
-	Platform,
+  NativeModules,
+  NativeEventEmitter,
+  DeviceEventEmitter,
+  NativeAppEventEmitter,
+  Platform,
 } from 'react-native';
 
 const { RNBackgroundTimer } = NativeModules;
 const Emitter = new NativeEventEmitter(RNBackgroundTimer);
 
 class BackgroundTimer {
+  constructor() {
+    this.uniqueId = 0;
+    this.callbacks = {};
 
-	constructor() {
-		this.uniqueId = 0;
-		this.callbacks = {};
+    Emitter.addListener('backgroundTimer.timeout', id => {
+      if (this.callbacks[id]) {
+        const callback = this.callbacks[id].callback;
+        if (!this.callbacks[id].interval) {
+          delete this.callbacks[id];
+        } else {
+          RNBackgroundTimer.setTimeout(id, this.callbacks[id].timeout);
+        }
+        callback();
+      }
+    });
+  }
 
-		Emitter.addListener('backgroundTimer.timeout', (id) => {
-			if (this.callbacks[id]) {
-				const callback = this.callbacks[id].callback;
-				if (!this.callbacks[id].interval) {
-					delete this.callbacks[id];
-				}
-				else {
-					RNBackgroundTimer.setTimeout(id, this.callbacks[id].timeout);
-				}
-				callback();
-			}
-		});
-	}
+  // Original API
+  start(delay = 0) {
+    return RNBackgroundTimer.start(delay);
+  }
 
-	// Original API
-	start(delay=0) {
-		return RNBackgroundTimer.start(delay);
-	}
+  stop() {
+    return RNBackgroundTimer.stop();
+  }
 
-	stop() {
-		return RNBackgroundTimer.stop();
-	}
+  runBackgroundTimer = (callback, delay) => {
+    const EventEmitter = Platform.select({
+      ios: () => NativeAppEventEmitter,
+      android: () => DeviceEventEmitter,
+    })();
+    this.start(0);
+    this.backgroundListener = EventEmitter.addListener(
+      'backgroundTimer',
+      () => {
+        this.backgroundListener.remove();
+        this.backgroundClockMethod(callback, delay);
+      }
+    );
+  };
 
-	runBackgroundTimer = (callback, delay) => {
-	  const EventEmitter = Platform.select({
-	    ios: () => NativeAppEventEmitter,
-	    android: () => DeviceEventEmitter,
-	  })();
-	  this.start(0);
-	  this.backgroundListener = EventEmitter.addListener('backgroundTimer', () => {
-	  	this.backgroundListener.remove();
-	    this.backgroundClockMethod(callback, delay)
-	  });
-	};
+  backgroundClockMethod(callback, delay) {
+    this.backgroundTimer = setTimeout(() => {
+      callback();
+      this.backgroundClockMethod(callback, delay);
+    }, delay);
+  }
 
-	backgroundClockMethod(callback, delay) {
-		this.backgroundTimer = setTimeout(() => {
-	    	callback();
-	    	this.backgroundClockMethod(callback, delay);
-	    }, 
-	    delay);
-	}
+  stopBackgroundTimer = () => {
+    this.stop();
+    clearTimeout(this.backgroundTimer);
+  };
 
-	stopBackgroundTimer = () => {
-	  this.stop();
-	  clearTimeout(this.backgroundTimer);
-	};
+  // New API, allowing for multiple timers
+  setTimeout(callback, timeout) {
+    const timeoutId = ++this.uniqueId;
+    this.callbacks[timeoutId] = {
+      callback: callback,
+      interval: false,
+      timeout: timeout,
+    };
+    RNBackgroundTimer.setTimeout(timeoutId, timeout);
+    return timeoutId;
+  }
 
-	// New API, allowing for multiple timers
-	setTimeout(callback, timeout) {
-		const timeoutId = ++this.uniqueId;
-		this.callbacks[timeoutId] = {
-			callback: callback,
-			interval: false,
-			timeout: timeout
-		};
-		RNBackgroundTimer.setTimeout(timeoutId, timeout);
-		return timeoutId;
-	}
+  clearTimeout(timeoutId) {
+    if (this.callbacks[timeoutId]) {
+      delete this.callbacks[timeoutId];
+    }
+  }
 
-	clearTimeout(timeoutId) {
-		if (this.callbacks[timeoutId]) {
-			delete this.callbacks[timeoutId];
-			//RNBackgroundTimer.clearTimeout(timeoutId);
-		}
-	}
+  setInterval(callback, timeout) {
+    const intervalId = ++this.uniqueId;
+    let expected = Date.now() + timeout;
 
-	setInterval(callback, timeout) {
-		const intervalId = ++this.uniqueId;
-		this.callbacks[intervalId] = {
-			callback: callback,
-			interval: true,
-			timeout: timeout
-		};
-		RNBackgroundTimer.setTimeout(intervalId, timeout);
-		return intervalId;
-	}
+    this.callbacks[intervalId] = {
+      callback: step,
+      interval: true,
+      timeout: timeout,
+    };
 
-	clearInterval(intervalId) {
-		if (this.callbacks[intervalId]) {
-			delete this.callbacks[intervalId];
-			//RNBackgroundTimer.clearTimeout(intervalId);
-		}
-	}
-};
+    const step = () => {
+      if (!this.callbacks[intervalId]) {
+        return;
+      }
+
+      const delta = Date.now() - expected;
+      const ticks = Math.max(1, 1 + Math.round(delta / timeout));
+      callback(ticks);
+      const addToExpected = timeout * ticks;
+      expected += addToExpected;
+
+      RNBackgroundTimer.setTimeout(intervalId, addToExpected - delta);
+    };
+
+    RNBackgroundTimer.setTimeout(intervalId, timeout);
+
+    return intervalId;
+  }
+
+  clearInterval(intervalId) {
+    if (this.callbacks[intervalId]) {
+      delete this.callbacks[intervalId];
+    }
+  }
+}
 
 export default new BackgroundTimer();
